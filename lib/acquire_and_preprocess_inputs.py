9d82fe7ebe468d8ac357ed2b26b29c40e0904122
BradfordBates-NOAA <bradford.bates@noaa.gov>
2020-06-05 13:09:14 -0500

    Added logic to pull and unzip NWM hydrofabric. Will need to refactor later to first check for more updated versions on shared drives, per conversation with FS.

--- lib/acquire_and_preprocess_inputs.py
+++ lib/acquire_and_preprocess_inputs.py
@@ -11,6 +11,7 @@ import sys
 from multiprocessing import Pool
 
 NUM_OF_WORKERS = 1
+
 from utils.shared_variables import (NHD_URL_PARENT,
                                     NHD_URL_PREFIX,
                                     NHD_RASTER_URL_SUFFIX,
@@ -19,10 +20,33 @@ from utils.shared_variables import (NHD_URL_PARENT,
                                     NHD_RASTER_EXTRACTION_SUFFIX,
                                     NHD_VECTOR_EXTRACTION_PREFIX,
                                     NHD_VECTOR_EXTRACTION_SUFFIX,
-                                    PREP_PROJECTION)
+                                    PREP_PROJECTION,
+                                    NWM_HYDROFABRIC_URL)
 
 from utils.shared_functions import pull_file
+    
 
+
+def pull_and_prepare_nwm_hydrofabric(path_to_saved_data_parent_dir):
+    
+    # -- Acquire and preprocess NWM data -- #
+    pull_and_prepare_nwm_hydrofabric()
+    nwm_hydrofabric_directory = os.path.join(path_to_saved_data_parent_dir, 'nwm_hydrofabric')
+    if not os.path.exists(nwm_hydrofabric_directory):
+        os.mkdir(nwm_hydrofabric_directory)
+    pulled_hydrofabric_tar_gz_path = os.path.join(nwm_hydrofabric_directory, 'NWM_channel_hydrofabric.tar.gz')
+    pull_file(NWM_HYDROFABRIC_URL, pulled_hydrofabric_tar_gz_path)
+    
+#    os.system("7za x {pulled_hydrofabric_tar_gz_path} -o{nwm_hydrofabric_directory}".format(pulled_hydrofabric_tar_gz_path=pulled_hydrofabric_tar_gz_path, nwm_hydrofabric_directory=nwm_hydrofabric_directory))
+    
+    pulled_hydrofabric_tar_path = pulled_hydrofabric_tar_gz_path.strip('.gz')
+#    os.system("7za x {pulled_hydrofabric_tar_path} -o{nwm_hydrofabric_directory}".format(pulled_hydrofabric_tar_path=pulled_hydrofabric_tar_path, nwm_hydrofabric_directory=nwm_hydrofabric_directory))
+    
+    os.remove(pulled_hydrofabric_tar_gz_path)
+    os.remove(pulled_hydrofabric_tar_path)
+    
+
+    
 
 def pull_and_prepare_nhd_data(procs_list):
     
@@ -37,29 +61,22 @@ def pull_and_prepare_nhd_data(procs_list):
     if not os.path.exists(nhd_raster_extraction_parent):
         os.mkdir(nhd_raster_extraction_parent)
     pull_file(nhd_raster_download_url, nhd_raster_extraction_path)
-#    pull_file(nhd_vector_download_url, nhd_vector_extraction_path)
-    print("All files pulled.")
+    pull_file(nhd_vector_download_url, nhd_vector_extraction_path)
     
-    # Unzip downloaded vector. Then delete the zipped dir.
+    # Unzip downloaded GDB.
     nhd_vector_extraction_parent = os.path.dirname(nhd_vector_extraction_path)
-    print(nhd_vector_extraction_parent)
-    huc = os.path.split(nhd_vector_extraction_parent)[1]
-#    os.system("7za x {nhd_vector_extraction_path} -o{nhd_vector_extraction_parent}".format(nhd_vector_extraction_path=nhd_vector_extraction_path, nhd_vector_extraction_parent=nhd_vector_extraction_parent))
-#    os.remove(nhd_vector_extraction_path)  # Delete the zipped GDB.
-#    
-    # Project and convert vectors to geopackage.
-    nhd_gdb = nhd_vector_extraction_path.replace('.zip', '.gdb')
-#    os.system("ogr2ogr -overwrite -progress -f GPKG -t_srs "$usgsProjection" $outputDataDir/WBDHU6_1209.gpkg $outputDataDir/NHDPLUS_H_1209_HU4_GDB.gdb WBDHU6"
-    os.system('ogr2ogr -overwrite -progress -f GPKG -t_srs "{PREP_PROJECTION}" {burn_line_event_gpkg} {nhd_gdb} NHDPlusBurnLineEvent'.format(PREP_PROJECTION=PREP_PROJECTION, burn_line_event_gpkg=os.path.join(nhd_vector_extraction_parent, 'NHDPlusBurnLineEvent_' + huc + '.gpkg'), nhd_gdb=nhd_gdb))
+    huc = os.path.split(nhd_vector_extraction_parent)[1]  # Parse HUC.
     
+    # Unzip vector and delete zipped file.
+    os.system("7za x {nhd_vector_extraction_path} -o{nhd_vector_extraction_parent}".format(nhd_vector_extraction_path=nhd_vector_extraction_path, nhd_vector_extraction_parent=nhd_vector_extraction_parent))
+    os.remove(nhd_vector_extraction_path)  # Delete the zipped GDB.
+    
+    # -- Project and convert NHDPlusBurnLineEvent and NHDPlusFlowLineVAA vectors to geopackage -- #
+    nhd_gdb = nhd_vector_extraction_path.replace('.zip', '.gdb')  # Update extraction path from .zip to .gdb. 
+    os.system('ogr2ogr -overwrite -progress -f GPKG -t_srs "{PREP_PROJECTION}" {burn_line_event_gpkg} {nhd_gdb} NHDPlusBurnLineEvent'.format(PREP_PROJECTION=PREP_PROJECTION, burn_line_event_gpkg=os.path.join(nhd_vector_extraction_parent, 'NHDPlusBurnLineEvent_' + huc + '.gpkg'), nhd_gdb=nhd_gdb))
     os.system('ogr2ogr -overwrite -progress -f GPKG -t_srs "{PREP_PROJECTION}" {flow_line_vaa_gpkg} {nhd_gdb} NHDPlusFlowlineVAA'.format(PREP_PROJECTION=PREP_PROJECTION, flow_line_vaa_gpkg=os.path.join(nhd_vector_extraction_parent, 'NHDPlusFlowlineVAA_' + huc + '.gpkg'), nhd_gdb=nhd_gdb))
     
     
-#             ogr2ogr -overwrite -progress -f GPKG -t_srs "$usgsProjection" $outputDataDir/NHDPlusFlowlineVAA_1209.gpkg $outputDataDir/NHDPLUS_H_1209_HU4_GDB.gdb NHDPlusFlowlineVAA
-    
-#        $outputDataDir/NHDPlusBurnLineEvent_1209.gpkg
-
-#ogr2ogr -overwrite -progress -f GPKG -t_srs "$usgsProjection" $outputDataDir/NHDPlusBurnLineEvent_1209.gpkg $outputDataDir/NHDPLUS_H_1209_HU4_GDB.gdb NHDPlusBurnLineEvent
 def manage_preprocessing(hucs_of_interest_file_path, path_to_saved_data_parent_dir):
     """
     This functions manages the downloading and preprocessing of gridded and vector data for FIM production.
@@ -70,7 +87,7 @@ def manage_preprocessing(hucs_of_interest_file_path, path_to_saved_data_parent_d
     Returns: TBD
     """
     
-    procs_list = []  # Initialize procs_list for multiprocessing.
+    nhd_procs_list = []  # Initialize procs_list for multiprocessing.
     
     # Create the parent directory if nonexistent.
     if not os.path.exists(path_to_saved_data_parent_dir):
@@ -82,7 +99,7 @@ def manage_preprocessing(hucs_of_interest_file_path, path_to_saved_data_parent_d
         os.mkdir(nhd_raster_dir)
         
     # Create the vector data parent directory if nonexistent.
-    vector_data_dir = os.path.join(path_to_saved_data_parent_dir, 'vector_data')
+    vector_data_dir = os.path.join(path_to_saved_data_parent_dir, 'nhdplus_vectors')
     if not os.path.exists(vector_data_dir):
         os.mkdir(vector_data_dir)
         
@@ -105,12 +122,18 @@ def manage_preprocessing(hucs_of_interest_file_path, path_to_saved_data_parent_d
             os.mkdir(nhd_vector_download_parent)
         nhd_vector_extraction_path = os.path.join(nhd_vector_download_parent, NHD_VECTOR_EXTRACTION_PREFIX + huc + NHD_VECTOR_EXTRACTION_SUFFIX)
 
-    
-        procs_list.append([nhd_raster_download_url, nhd_raster_extraction_path, nhd_vector_download_url, nhd_vector_extraction_path])
+        # Append extraction instructions to nhd_procs_list.
+        nhd_procs_list.append([nhd_raster_download_url, nhd_raster_extraction_path, nhd_vector_download_url, nhd_vector_extraction_path])
         
-    # Pull NHD Data.
-    pool = Pool(NUM_OF_WORKERS)
-    pool.map(pull_and_prepare_nhd_data, procs_list)
+#    # Pull NHD Data.
+#    pool = Pool(NUM_OF_WORKERS)
+#    pool.map(pull_and_prepare_nhd_data, nhd_procs_list)
+    
+    pull_and_prepare_nwm_hydrofabric(path_to_saved_data_parent_dir)
+
+    
+        
+    
 
 
 if __name__ == '__main__':
@@ -118,6 +141,9 @@ if __name__ == '__main__':
     # Get input arguments from command line.
     hucs_of_interest_file_path = sys.argv[1]
     path_to_saved_data_parent_dir = sys.argv[2]  # The parent directory for all saved data.
+#    pull_nwm_hydrofabric = sys.argv[3]
+    
+    # 
     
     manage_preprocessing(hucs_of_interest_file_path, path_to_saved_data_parent_dir)
 
--- lib/utils/shared_variables.py
+++ lib/utils/shared_variables.py
@@ -10,9 +10,11 @@ Created on Thu Jun  4 11:00:02 2020
 PREP_PROJECTION = '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m no_defs'  # from preprocess_gis.sh
 
 
-# -- Data -- #
-# Gridded data download URLs and path extraction strings.
+# -- Data URLs-- #
 NHD_URL_PARENT = r'ftp://rockyftp.cr.usgs.gov/vdelivery/Datasets/Staged/Hydrography/NHDPlus/HU4/HighResolution/GDB/'
+NWM_HYDROFABRIC_URL = r'http://www.nohrsc.noaa.gov/pub/staff/keicher/NWM_live/web/data_tools/NWM_channel_hydrofabric.tar.gz'  # Temporary
+
+# -- Prefixes and Suffixes -- #
 NHD_URL_PREFIX = 'NHDPLUS_H_'
 NHD_RASTER_URL_SUFFIX = '_HU4_RASTER.7z'
 NHD_VECTOR_URL_SUFFIX = '_HU4_GDB.zip'
