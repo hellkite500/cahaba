99d13090a12decb680d392a9481ba5f5d109e686
BrianAvant-NOAA <brian.avant@noaa.gov>
2020-06-17 13:54:20 +0000

    addressed comments on ticket; ready to merge into dev

--- lib/split_flows.py
+++ lib/split_flows.py
@@ -42,11 +42,6 @@ lakes = gpd.read_file(lakes_filename)
 WBD8 = gpd.read_file(huc8_filename)
 dem = Raster(dem_fileName)
 
-# Check projection
-WBD8 = WBD8.to_crs(projection)
-flows = flows.to_crs(projection)
-lakes = lakes.to_crs(projection)
-
 WBD8 = WBD8.filter(items=['fossid', 'geometry'])
 WBD8 = WBD8.set_index('fossid')
 flows = flows.explode()
